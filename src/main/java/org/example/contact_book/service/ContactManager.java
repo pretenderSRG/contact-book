c3e5303fffd3c64fe87e8c552dc8b81e3d571141
Serg Pretender <sergdynamite@gmail.com>
2025-04-02 18:39:07 +0300

    complete models/Contact

--- src/main/java/org/example/contact_book/service/ContactManager.java
+++ src/main/java/org/example/contact_book/service/ContactManager.java
@@ -1,4 +1,9 @@
 package org.example.contact_book.service;
 
+import org.example.contact_book.model.Contact;
+
+import java.util.Map;
+
 public class ContactManager {
+    private Map<String, Contact> contactStorage;
 }

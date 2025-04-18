c1fbcc3299644ccb8e3063f98d212df53142eb77
Serg Pretender <sergdynamite@gmail.com>
2025-04-18 14:01:56 +0300

    Finish testing ContactEditor

---
+++ src/test/java/org/example/contact_book/service/ContactValidatorTest.java
@@ -0,0 +1,16 @@
+package org.example.contact_book.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContactValidatorTest {
+
+    @Test
+    void isPhoneNumberValid() {
+    }
+
+    @Test
+    void isEmailValid() {
+    }
+}
